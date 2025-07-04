6854c581dfb7e608be2d80ec2444f20421e4f3a5
Ian Wodder <ian.wodder@gmail.com>
2025-07-04 13:25:22 -0600

    Complete polynomial portion of exercise

--- module-5/calculator/src/main/scala/calculator/Polynomial.scala
+++ module-5/calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,8 +3,17 @@ package calculator
 object Polynomial extends PolynomialInterface:
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] =
-    ???
+    Signal {
+      Math.pow(b(), 2.0) - 4 * a() * c()
+    }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] =
-    ???
+    Signal {
+      if delta() < 0 then Set.empty
+      else if delta() == 0 then Set(-(b()/(2*a())))
+      else
+        val first = (-b() + Math.sqrt(delta())) / (2 * a())
+        val second = (-b() - Math.sqrt(delta())) / (2 * a())
+        Set(first, second)
+    }
