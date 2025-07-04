f118cff627269713f8dd4ceacf25abeefb538a1d
Ian Wodder <ian.wodder@gmail.com>
2025-07-04 14:09:03 -0600

    Complete implementation of calculator

--- module-5/calculator/src/main/scala/calculator/Calculator.scala
+++ module-5/calculator/src/main/scala/calculator/Calculator.scala
@@ -13,10 +13,24 @@ object Calculator extends CalculatorInterface:
 
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] =
-    ???
+    namedExpressions.map{
+      (name, expr) => 
+        name -> Signal {
+          eval(expr(), namedExpressions - name)
+        }
+    }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]])(using Signal.Caller): Double =
-    ???
+    expr match
+      case Literal(x) => x
+      case Ref(name) => {
+        eval(getReferenceExpr(name, references), references)
+      }
+      case Plus(a, b) => eval(a, references) + eval(b, references)
+      case Minus(a, b) => eval(a, references) - eval(b, references)
+      case Times(a, b) => eval(a, references) * eval(b, references)
+      case Divide(a, b) => eval(a, references) / eval(b, references)
+    
 
   /** Get the Expr for a referenced variables.
    *  If the variable is not known, returns a literal NaN.
